fb585f6ad5a55958720a2c5cc54e390bda59ee3b
Sam Beveridge <sam.bev87@gmail.com>
2014-10-06 00:01:34 -0600

    Removing some unneeded files

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, Response, render_template, make_response
 from app.blueprints.main import main
 from app.blueprints.trial import TrialREST
-from app.blueprints.rider import RiderREST
 from config.jinjacfg import setUpJinjaEnv
 from config.settings import SETTINGS
 from flask.ext.restful import Api
@@ -14,7 +13,7 @@ app.config.update(SETTINGS['dev'])
 
 app.register_blueprint(main)
 api.add_resource(TrialREST, '/trials/<string:id>', '/trials/')
-api.add_resource(RiderREST, '/riders/<string:id>', '/riders/')
+
 
 @api.representation('text/html')
 def output_html(data, code, headers=None):
